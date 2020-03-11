65658bd3098470a84aadc9c6006293551fda19a6
Ben Lane <robo.guy@outlook.com>
2020-03-11 21:24:54 +0800

    Fixed Section backgrouunds + navbar completed

--- js/nav.js
+++ js/nav.js
@@ -1,22 +1,24 @@
+heights = [];
+for (i = 0, len = document.getElementsByClassName("section").length; i < len; i++) {
+    div = document.getElementsByClassName("section")[i]
+    heights.push(div.offsetHeight)
+}
+updateDivs()
 function updateDivs() {
 
     height = document.getElementById("headerbar").offsetHeight;
     
     for (i = 0, len = document.getElementsByClassName("section").length; i < len; i++) {
         div = document.getElementsByClassName("section")[i]
-        div.style.paddingTop = height.toString() + "px";
-        console.log(height)
-    }
-    for (i = 0, len = document.getElementsByClassName("innersection").length; i < len; i++) {
-        div = document.getElementsByClassName("section")[i]
         div.style.marginTop = "-" + height.toString() + "px";
     }
-  }
+    for (i = 0, len = document.getElementsByClassName("section_title").length; i < len; i++) {
+        div = document.getElementsByClassName("section_title")[i]
+        div.style.paddingTop = height.toString() + "px";
+    }
+}
 
 for (i = 0, len = document.getElementsByClassName("navbarlink").length; i < len; i++) {
-console.log("run")
-button = document.getElementsByClassName("navbarlink")[i]
-button.onclick = function(){updateDivs()};
+    button = document.getElementsByClassName("navbarlink")[i]
+    button.onclick = function(){updateDivs()};
 }
-  
-  
